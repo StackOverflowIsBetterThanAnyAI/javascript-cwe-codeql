a70e94ee6a8927765868b8d5f16a44f805226ec7
Michael Münzenhofer <140268904+StackOverflowIsBetterThanAnyAI@users.noreply.github.com>
2025-02-11 23:25:30 +0100

    revert change

--- CWE-312/main.js
+++ CWE-312/main.js
@@ -1,4 +1,6 @@
-const main = () => {
-    const password = sessionStorage.getItem('vulnerable')
-    sessionStorage.setItem('vulnerable', password)
+const main = (key, value) => {
+    const storage = sessionStorage.getItem('vulnerable')
+    const item = storage ? JSON.parse(storage) : {}
+    item[key] = value
+    sessionStorage.setItem('vulnerable', JSON.stringify(item))
 }
