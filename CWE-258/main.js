7f80aa3cf8cb47f8a1c9475525731badc707c03c
Michael Münzenhofer <michael.muenzenhofer@gmail.com>
2025-02-12 01:23:12 +0100

    empty string

--- CWE-258/main.js
+++ CWE-258/main.js
@@ -1,10 +1,8 @@
-const DB_PASSWORD = 'password1234'
-
 const main = () => {
     const dbConfig = {
         host: 'localhost',
         user: 'admin',
-        password: DB_PASSWORD,
+        password: '',
     }
 
     connectToDataBase(dbConfig)
